e2380d21fc33623b7d92ce19cf575aca89a7c64c
Dikezeko Oliveira <dikezeko.oliveira@firgun.com.br>
2021-03-30 17:46:32 -0300

    TransactionsTable

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,34 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+          <th>Título</th>
+          <th>Valor</th>
+          <th>Categoria</th>
+          <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/03/2021</td>
+          </tr>
+          <tr>
+            <td>Alugel</td>
+            <td className="withdraw">- R$ 2.000,00</td>
+            <td>Alugel</td>
+            <td>20/03/2021</td>
+          </tr>
+
+        </tbody>
+      </table>
+    </Container>
+  );
+}
